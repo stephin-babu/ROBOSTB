40fa82706f8e48383a4c8d3320f57d833534f9bd
stephin-babu <stephinbabu789@gmail.com>
2024-11-17 10:30:48 +0530

    1ST DATA SET

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to scroll to a specific section
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
